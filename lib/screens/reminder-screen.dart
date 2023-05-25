03c8a9573721e2047e093fa8b3aa3544b6fe794e
nishantwillow <tanksalenishant89@gmail.com>
2023-05-26 00:41:06 +0530

    reminder working with deletion

--- lib/screens/reminder-screen.dart
+++ lib/screens/reminder-screen.dart
@@ -5,6 +5,30 @@ import 'package:medihelp/main.dart';
 import '/components/alarm-info.dart';
 import 'package:timezone/timezone.dart' as tz;
 
+import 'add-reminder.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Meditation App',
+      theme: ThemeData(
+        fontFamily: "SourceSansPro",
+        scaffoldBackgroundColor: Color(0xFFA7B2D0),
+        //textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
+      ),
+      home: ReminderPage(),
+    );
+  }
+}
+
 class ReminderPage extends StatefulWidget {
   const ReminderPage({Key? key}) : super(key: key);
 
@@ -15,24 +39,33 @@ class ReminderPage extends StatefulWidget {
 class _ReminderPageState extends State<ReminderPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
+    return Scaffold(
+      body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           SizedBox(
-            height: 30,
+            height: 40,
           ),
           const Text(
             "Reminders",
             style: TextStyle(
               fontWeight: FontWeight.w700,
-              color: Colors.white,
+              color: Colors.black,
               fontSize: 24,
+              fontFamily: "SourceSansPro",
             ),
           ),
           Expanded(
             child: ListView(
               children: alarmList.map((alarm) {
+                String amOrPm;
+                if (alarm.alarmTime.hour > 11) {
+                  amOrPm = "pm";
+                } else {
+                  amOrPm = "am";
+                  print(alarm.alarmId);
+                }
+
                 return Container(
                   margin: const EdgeInsets.only(bottom: 36),
                   padding:
@@ -63,7 +96,7 @@ class _ReminderPageState extends State<ReminderPage> {
                               ),
                               SizedBox(width: 8),
                               Text(
-                                alarm.description!,
+                                alarm.description,
                                 style: TextStyle(
                                     color: Colors.white, fontFamily: 'avenir'),
                               ),
@@ -71,25 +104,30 @@ class _ReminderPageState extends State<ReminderPage> {
                           ),
                         ],
                       ),
-                      Text(
-                        'Mon-Fri',
-                        style: TextStyle(
-                            color: Colors.white, fontFamily: 'avenir'),
-                      ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          const Text(
-                            "alarmTime",
+                          Text(
+                            alarm.alarmTime.hour.toString() +
+                                " : " +
+                                alarm.alarmTime.minute.toString() +
+                                "  " +
+                                amOrPm,
                             style: TextStyle(
                                 color: Colors.white,
                                 fontFamily: 'avenir',
                                 fontSize: 24,
                                 fontWeight: FontWeight.w700),
                           ),
-                          Icon(
-                            Icons.delete,
-                            color: Colors.white,
+                          TextButton(
+                            child: Icon(
+                              Icons.delete,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              deleteNotification(alarm.alarmId);
+                              removeElementFromList(alarm.alarmId);
+                            },
                           ),
                         ],
                       ),
@@ -111,7 +149,13 @@ class _ReminderPageState extends State<ReminderPage> {
                     ),
                     child: TextButton(
                       onPressed: () {
-                        scheduleNotification();
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const datetime(),
+                          ),
+                        );
+                        //scheduleNotification();
                       },
                       child: Column(
                         children: <Widget>[
@@ -142,40 +186,58 @@ class _ReminderPageState extends State<ReminderPage> {
       ),
     );
   }
+}
 
-  void scheduleNotification() async {
-    var dateTime = DateTime.now().add(
-      Duration(seconds: 10),
-    );
-    String timeZone = 'Asia/Kolkata';
-    var scheduleNotifcationDateTime =
-        tz.TZDateTime.from(dateTime, tz.getLocation(timeZone));
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-      'alarm_notif',
-      'alarm_notif',
-      channelDescription: 'Channel for Alarm notification',
-      icon: 'medihelp_logo',
-      largeIcon: DrawableResourceAndroidBitmap('medihelp_logo'),
-    );
-    var iOSPlatformChannelSpecifics = DarwinNotificationDetails(
-      sound: 'a_long_cold_sting.wav',
-      presentAlert: true,
-      presentBadge: true,
-      presentSound: true,
-    );
-    var platformChannelSpecifics = NotificationDetails(
-      android: androidPlatformChannelSpecifics,
-      iOS: iOSPlatformChannelSpecifics,
-    );
-    notificationPlugin = FlutterLocalNotificationsPlugin();
-    await notificationPlugin.zonedSchedule(
-      0,
-      "Demo",
-      "Hello!",
-      scheduleNotifcationDateTime,
-      platformChannelSpecifics,
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.wallClockTime,
-    );
+void scheduleNotification(DateTime dt, int notifID, String description) async {
+  String timeZone = 'Asia/Kolkata';
+  var scheduleNotifcationDateTime =
+      tz.TZDateTime.from(dt, tz.getLocation(timeZone));
+  var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    'alarm_notif',
+    'alarm_notif',
+    channelDescription: 'Channel for Alarm notification',
+    icon: 'medihelp_logo',
+    largeIcon: DrawableResourceAndroidBitmap('medihelp_logo'),
+  );
+  var iOSPlatformChannelSpecifics = DarwinNotificationDetails(
+    sound: 'a_long_cold_sting.wav',
+    presentAlert: true,
+    presentBadge: true,
+    presentSound: true,
+  );
+  var platformChannelSpecifics = NotificationDetails(
+    android: androidPlatformChannelSpecifics,
+    iOS: iOSPlatformChannelSpecifics,
+  );
+  notificationPlugin = FlutterLocalNotificationsPlugin();
+  await notificationPlugin.zonedSchedule(
+    notifID,
+    description,
+    "",
+    scheduleNotifcationDateTime,
+    platformChannelSpecifics,
+    uiLocalNotificationDateInterpretation:
+        UILocalNotificationDateInterpretation.wallClockTime,
+  );
+}
+
+void deleteNotification(int notifID) async {
+  await notificationPlugin.cancel(
+    notifID,
+  );
+  print("deleted id\n");
+  print(notifID);
+}
+
+void removeElementFromList(int id) {
+  int index = -1;
+  for (int i = 0; i < alarmList.length; i++) {
+    if (alarmList[i].alarmId == id) {
+      index = i;
+      break;
+    }
+  }
+  if (index != -1) {
+    alarmList.removeAt(index);
   }
 }
